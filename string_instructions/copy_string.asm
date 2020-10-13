7224818c557d31e58455a194068ae9b2718417b1
Cory Nance <cnance@citadel.edu>
2020-10-13 14:20:53 -0400

    Added movsb example.

---
+++ string_instructions/copy_string.asm
@@ -0,0 +1,26 @@
+section .data
+str1: db "Hello, world!", 0xa, 0
+str1_len: equ ($-str1)
+
+section .bss
+str2: resb str1_len
+
+section .text
+global main
+extern printf
+extern exit 
+
+main:
+
+mov esi, str1 ; set the source
+mov edi, str2 ; set the destination
+mov ecx, str1_len ; number of times to repeat
+rep movsb ; rep instruction movs bytes
+
+; print str2
+push str2
+call printf
+sub esp, 4
+
+push 0
+call exit
